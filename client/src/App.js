ce67c2e67496be31f3d53e06ecbb8d6bcf5b9d55
sukaran <sukarankant765@gmail.com>
2022-12-27 22:22:20 +0530

    add signup page

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <>
       <Navbar/>
       {/* <Home/> */}
-      <Login/>
+      {/* <Login/> */}
+      <Signup/>
     </>
   );
 }
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -60,10 +60,7 @@ export default function Login() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
+         
             <Button
               type="submit"
               fullWidth
