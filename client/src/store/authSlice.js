a1159c69b02ef661617e04d0aabc3b6641d375b3
sukaran <sukarankant765@gmail.com>
2022-12-28 09:42:27 +0530

    add store for redux

---
+++ client/src/store/authSlice.js
@@ -0,0 +1,49 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    isAuthenticated: false,
+    user: null,
+    error: null,
+    loading: false,
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginSuccess(state, action) {
+      state.isAuthenticated = true;
+      state.user = action.payload;
+      state.error = null;
+      state.loading = false;
+    },
+    loginError(state, action) {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    logoutSuccess(state) {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.error = null;
+      state.loading = false;
+    },
+    logoutError(state, action) {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+  extraReducers: {
+    // [login.pending]: (state) => {
+    //   state.loading = true;
+    //   state.error = null;
+    // },
+    // [logout.pending]: (state) => {
+    //   state.loading = true;
+    //   state.error = null;
+    // },
+  },
+})
+
+// export const { } = authSlice.actions
+
+export default authSlice.reducer
