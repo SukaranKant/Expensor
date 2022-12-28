bc70e890192ab21329c33eb6ca6f78f5be713cd5
sukaran <sukarankant765@gmail.com>
2022-12-28 19:49:49 +0530

    add logout handler

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,9 +4,22 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store/authSlice';
+import Cookies from 'js-cookie';
 
 export default function Navbar() {
+  const dispatch = useDispatch()
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated)
+  const navigate = useNavigate()
+
+  const logoutHandler = ()=>{
+    Cookies.remove('auth-token')
+    dispatch(authActions.logoutSuccess())
+    navigate('/login')
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -14,9 +27,9 @@ export default function Navbar() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Expensor
           </Typography>
-          <Button color="inherit"><Link to="/login" style={{color: 'inherit', textDecoration: 'inherit'}}>Login</Link></Button>
-          <Button color="inherit"><Link to="/signup" style={{color: 'inherit', textDecoration: 'inherit'}}>Signup</Link></Button>
-          <Button color="inherit">Logout</Button>
+          {!isAuthenticated && <Button color="inherit"><Link to="/login" style={{color: 'inherit', textDecoration: 'inherit'}}>Login</Link></Button>}
+          {!isAuthenticated && <Button color="inherit"><Link to="/signup" style={{color: 'inherit', textDecoration: 'inherit'}}>Signup</Link></Button>}
+          {isAuthenticated && <Button color="inherit" onClick={logoutHandler}>Logout</Button>}
         </Toolbar>
       </AppBar>
     </Box>
